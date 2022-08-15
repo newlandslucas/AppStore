d91a1f1e632eefe7c004e5f0faf7fbf1144e7792
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2022-08-14 22:58:32 -0300

    feat: start project

---
+++ AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by Lucas Newlands on 14/08/22.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
